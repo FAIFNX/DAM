97921c3c5f39e1de46cfbd4e77504c956db7c38e
FAIFNX <pol.c.dpg@gmail.com>
2024-10-07 21:26:45 +0200

    1

--- Projectos/Shockets/Servidor.py
+++ Projectos/Shockets/Servidor.py
@@ -1,5 +1,4 @@
 import socket
-import os
 
 def start_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,15 +14,15 @@ def start_server(host, port):
         operation = conn.recv(1024).decode()
         if operation == "send_file":
             filename = conn.recv(1024).decode()
-            # Guardar el archivo en el mismo directorio donde está el script
-            with open(os.path.join(os.getcwd(), filename), 'wb') as f:
-                print(f'Recibiendo archivo: {filename}')
+            file_path = os.path.join(data_directory, filename)
+            with open(file_path, 'wb') as f:
+                print(f'Recibiendo archivo: {file_path}')
                 while True:
                     data = conn.recv(1024)
                     if not data:
                         break
                     f.write(data)
-            print(f'Archivo {filename} recibido.')
+            print(f'Archivo {file_path} recibido.')
 
         elif operation == "send_text":
             message = conn.recv(1024).decode()
@@ -37,4 +36,4 @@ def start_server(host, port):
     server_socket.close()
 
 if __name__ == "__main__":
-    start_server('localhost', 65432)  # Cambia 'localhost' por la IP de tu servidor.
+    start_server('localhost', 65432)  # Cambia 'localhost' por la IP de tu servidor
